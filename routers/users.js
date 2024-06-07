a8513c4335cc5b92620ecce3668064ef2c7cca15
Said Masih <saidmsajady@gmail.com>
2024-06-07 12:08:41 -0700

    Finished video

--- routers/users.js
+++ routers/users.js
@@ -1,21 +1,33 @@
 const express = require(`express`);
 const router = express.Router();
 
+router.use(logger);
+
 router.get(`/`, (req, res) => {
     res.send(`User List`);
 })
 
 router.get(`/new`, (req, res) => {
-    res.send(`User New Form`);
+    res.render(`users/new`)
 })
 
 router.post(`/`, (req, res) => {
-    res.send(`Create User`);
+    const isValid = true;
+    if (isValid) {
+        users.push({firstName: req.body.firstName});
+        res.redirect(`/users/${users.length - 1}`);
+    } else {
+        console.log(`Error`);
+        res.render(`users/new`, {firstName: req.body.firstName})
+    }
+    console.log(res.render(`users/new`));
+    res.send("Hi")
 })
 
 router
     .route(`/:id`)
     .get((req, res) => {
+        console.log(req.user)
         res.send(`Get user Get with ID ${req.params.id}`);
     })
     .put((req, res) => {
@@ -25,8 +37,15 @@ router
         res.send(`Delete user Get with ID ${req.params.id}`);
     })
 
+const users = [{name: "Kyle"}, {name: "Sally"}]
 router.param(`id`, (req, res, next, id) => {
-    console.log(id);
+    req.user = users[id];
+    next();
 })
+
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const app = express();
 
 app.listen(3000);
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended:true }))
+
 app.set(`view engine`, `ejs`);
 
-app.get(`/`, (req, res) => {
-    console.log(`here`);
-    res.render(`index`, { text:  `World`});
-    // res.status(500).json({message: `Error`}) 
-    // res.download(`server.js`);
-})
+// app.get(`/`, (req, res) => {
+//     res.render(`index`, { text:  `World`});
+//     res.status(500).json({message: `Error`}) 
+//     res.download(`server.js`);
+// })
 
 const userRouter = require(`./routers/users`);
 app.use(`/users`, userRouter);
